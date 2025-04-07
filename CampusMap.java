725033017d56692fefb5595c4639db676e8f67eb
cyang-05 <chiashiiyang@gmail.com>
2025-04-07 11:36:04 -0400

    reflection

--- CampusMap.java
+++ CampusMap.java
@@ -44,16 +44,18 @@ public class CampusMap {
         CampusMap myMap = new CampusMap();
         myMap.addBuilding(new Building("Ford Hall", "100 Green Street Northampton, MA 01063", 4));
         myMap.addBuilding(new Building("Bass Hall", "4 Tyler Court Northampton, MA 01063", 4));
-        myMap.addBuilding(new Building("Cutter House", "10 Prospect St, Northampton, MA 01060", 4));
+        myMap.addBuilding(new House("Cutter House", "10 Prospect St, Northampton, MA 01060", 4, true));
         myMap.addBuilding(new Building("Julia Child Campus Center", " Smith College Campus Center, 100 Elm St, Northampton, MA 01063", 3));
         myMap.addBuilding(new Building("Mwangi Cultural Center", "12 Prospect St, Northampton, MA 01063", 1));
-        myMap.addBuilding(new Building("Wilson House", "1 Paradise Rd, Northampton, MA 01063", 5));
+        myMap.addBuilding(new House("Wilson House", "1 Paradise Rd, Northampton, MA 01063", 5, true));
         myMap.addBuilding(new Building("Jandon Center", "College Ln, Northampton, MA 01063", 1));
         myMap.addBuilding(new Building("Emerson House", "1 Paradise Rd, Northampton, MA 01063", 5));
-        myMap.addBuilding(new Building("Haven-Wesley", "1 Random Rd, Northampton, MA 01063", 4));
-        myMap.addBuilding(new Building("Lamont", "17 Prospect St, Northampton, MA 01063", 4));
+        myMap.addBuilding(new House("Haven-Wesley", "1 Random Rd, Northampton, MA 01063", 4, true));
+        myMap.addBuilding(new House("Lamont", "17 Prospect St, Northampton, MA 01063", 4, true));
         myMap.addBuilding(new Building("McConnell Hall", "1 Science St, Northampton, MA 01063", 4));
-        myMap.addBuilding(new Building("President's House", "1 Presidential St, Northampton, MA 01063", 3));
+        myMap.addBuilding(new House("President's House", "1 Presidential St, Northampton, MA 01063", 3, true));
+        myMap.addBuilding(new House("Ziskind House", "1 Henshaw Ave, Northampton, MA 01063", 4, true));
+        myMap.addBuilding(new Library("Neilson Library", "7 Neilson Drive, Northampton, MA 01063", 5));
         
         System.out.println(myMap);
     }
--- reflection.md
+++ reflection.md
@@ -2,3 +2,5 @@ Use this file to record your reflection on this assignment.
 
 - Which methods did you decide to `overload`, and why?
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+Chiashi Yang - The methods we decided to overload for each subclass were the constructors because this allows for a specific building to be initialized differently based on the parameters provided. Thus, making the initialization process more flexible. The CampusMap worked well, as we were able to initialize different buildings. In addition, overloading the constructors worked well. With this assignment, I was still having trouble with when to implement super and when not to. I think with practice I will know when to use it. Some advice I would give to someone taking this course in the future is to ask questions and start the assignment early!
